d53840a8a45fa83d426b88b7e255fdc796dde916
alenash1988 <Aleonash1988@mail.ru>
2024-08-14 23:10:56 +0900

    Add files via upload

---
+++ modui_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('В каком городе ты живешь?: ')
+print(my_string.upper())
+print(my_string.lower())
+print("В каком городе ты живешь?".replace(' ',''))
+my_string = "В каком городе ты живешь?"
+print(my_string[0])
+print(my_string[-1])
